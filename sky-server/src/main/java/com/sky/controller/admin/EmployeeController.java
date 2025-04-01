283305476f2d6cd8fc43189f28108e4712315897
liu <123@.com>
2025-04-01 18:09:45 +0800

    编辑员工信息相关代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -107,10 +107,27 @@ public class EmployeeController {
      * @return
      */
     @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
     public Result startOrStop(@PathVariable Integer status,Long id)
     {
         log.info("启用禁用员工账号:{},{}",status,id);
         employeeService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(("根据id查询员工数据"))
+    public Result<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    @ApiOperation("员工编辑")
+    public Result update(@RequestBody EmployeeDTO employeeDTO)
+    {
+        log.info("编辑员工信息：:{}",employeeDTO);
+        employeeService.update(employeeDTO);
         return Result.success();
     }
 
